89e8666ede040cc301989b04417d4909e3127aea
Jared Benskins <jaredbenskins@gmail.com>
2019-05-26 23:07:01 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor = "#fff";
+let defaultStyle = {
+  color: defaultTextColor,
+
+};
+
+class Agg extends Component {
+  render() {
+    return (
+      <div style = {{...defaultStyle, width: "40%", display: 'inline-block'}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style = {defaultStyle}>
+        <img/>
+        <input type = "text"/>
+      </div>
+    );
+  }
+}
+class Playlist extends Component {
+  render() {
+    return(
+      <div style = {{...defaultStyle, width: "20%", display: "inline-block"}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+  render() {
+    let name = "Jared"
+    let headerStyle = {color: "red", "font-size": "50px"}
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Agg/>
+        <Agg/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
+}
+
+export default App;
